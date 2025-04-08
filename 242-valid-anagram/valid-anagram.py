52d1000aab8573c2510377b768936da013819e04
Aniruddha Aranya A Dheu <125354418+aranyaadheu@users.noreply.github.com>
2025-04-08 19:33:06 +0600

    Time: 7 ms (89.45%) | Memory: 17.8 MB (86.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Make sure the strings have same lengths. 
-        if len(s) != len(t):
-            return False
-        # Sort both strings and compare them.
-        return sorted(s) == sorted(t)
+        count = defaultdict(int)
+        for x in s:
+            count[x] += 1
+        for x in t:
+            count[x] -= 1
+        for val in count.values():
+            if val != 0:
+                return False
+        return True
